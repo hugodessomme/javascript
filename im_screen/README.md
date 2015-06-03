ac0ee18341bd080cb7fce3c9841bc4ab5e680613
hugodessomme <hdessomme49@hotmail.fr>
2015-06-03 22:36:10 +0200

    im_screen folder added

---
+++ im_screen/README.md
@@ -0,0 +1,31 @@
+#im_screen.js
+
+**Description** : *Génération d'une classe sur l'élément `html` suivant les 4 breakpoints définis (une 5ème classe est générée au-dessus de `big_breakpoint`).*
+
+##Aperçu
+
+	im_screen(tiny_breakpoint, medium_breakpoint, large_breakpoint, big_breakpoint) 
+
+##Utilisation (exemple)
+	
+###jQuery
+
+	$(document).ready(function(){
+		im_screen(480, 640, 768, 1024);
+	});
+
+##Arguments
+
+* `tiny_breakpoint` : integer
+* `medium_breakpoint` : integer
+* `large_breakpoint` : integer
+* `big_breakpoint` : integer
+
+
+##Fonctionnement
+
+1. si la largeur d'écran est **inférieure ou égale** à `tiny_breakpoint`, génération de la classe `is-sm-screen`
+2. si la largeur d'écran est **supérieure** à `tiny_breakpoint` et **inférieure ou égale** à `medium_breakpoint`, génération de la classe `is-md-screen`
+3. si la largeur d'écran est **supérieure** à `medium_breakpoint` et **inférieure ou égale** à `large_breakpoint`, génération de la classe `is-lg-screen`
+4. si la largeur d'écran est **supérieure** à `large_breakpoint` et **inférieure ou égale** à `big_breakpoint`, génération de la classe `is-bg-screen`
+5. si la largeur d'écran est **supérieure ou égale** à `big_breakpoint`, génération de la classe `is-max-screen`
---
+++ im_screen/im_screen.js
@@ -0,0 +1,55 @@
+function im_screen(tiny_breakpoint, medium_breakpoint, large_breakpoint, big_breakpoint) {
+    $('html').each(function(){
+        var $this = $(this),
+            ww_w = $(window).width();
+
+        if(typeof tiny_breakpoint === 'undefined' || typeof medium_breakpoint === 'undefined' || typeof large_breakpoint === 'undefined' || typeof big_breakpoint === 'undefined') { 
+            console.log('One or several "XXX_breakpoint" parameters are missing or empty, please add/fill first :)'); 
+        }
+
+        // si on est au tiny_breakpoint
+        if(ww_w <= tiny_breakpoint) {
+            if(!$this.hasClass('is-sm-screen')){
+                $this
+                    .addClass('is-sm-screen')
+                    .removeClass('is-md-screen is-lg-screen is-bg-screen is-max-screen');
+            }
+        }
+
+        // si on est au medium_breakpoint
+        else if(ww_w > tiny_breakpoint && ww_w <= medium_breakpoint) {
+            if(!$this.hasClass('is-md-screen')){
+                $this
+                    .addClass('is-md-screen')
+                    .removeClass('is-sm-screen is-lg-screen is-bg-screen is-max-screen');
+            }
+        }
+
+        // si on est au large_breakpoint
+        else if(ww_w > medium_breakpoint && ww_w <= large_breakpoint) {
+            if(!$this.hasClass('is-lg-screen')){
+                $this
+                    .addClass('is-lg-screen')
+                    .removeClass('is-sm-screen is-md-screen is-bg-screen is-max-screen');
+            }
+        }
+
+        // si on est au big_breakpoint
+        else if(ww_w > large_breakpoint && ww_w <= big_breakpoint) {
+            if(!$this.hasClass('is-bg-screen')){
+                $this
+                    .addClass('is-bg-screen')
+                    .removeClass('is-sm-screen is-md-screen is-lg-screen is-max-screen');
+            }
+        }
+
+        // si on est au-dessus du big_breakpoint
+        else {
+            if(!$this.hasClass('is-max-screen')){
+                $this
+                    .addClass('is-max-screen')
+                    .removeClass('is-sm-screen is-md-screen is-lg-screen is-bg-screen');
+            }
+        }
+    });
+}
